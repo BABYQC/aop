4f71c32d0d86fe0e55aa228428b3c9c0a6c285bd
changqi <649620862@qq.com>
2024-12-09 11:18:13 +0800

    hashmap测试

---
+++ src/main/java/com/example/aop/demo/HashMapTest.java
@@ -0,0 +1,18 @@
+package com.example.aop.demo;
+
+import java.util.HashMap;
+
+/**
+ * @Author: chang qi
+ * @Date: 2024/11/27
+ */
+public class HashMapTest {
+    public static void main(String[] args) {
+//        HashMap.
+        HashMap<String, String> map = new HashMap<>();
+
+//        short s = 1;
+//        s = s + 1;
+    }
+
+}
